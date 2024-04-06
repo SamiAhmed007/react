34a1bac64c79212764cf16bd571c3bb310f2f983
SamiAhmed007 <samiahmedkhan58@gmail.com>
2024-04-06 17:58:30 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <HelloWorld name={'HEMALATHA CAROLIN PALAPARTHI'} />
+        <HelloWorld name={'Mohammed Sami Ahmed'} />
         <br />
         <Counter />
         <hr />
